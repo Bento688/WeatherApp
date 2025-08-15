daced2fc2ff1599599c9cade17f99e859858f030
Bento688 <benedict.halkin688@gmail.com>
2025-08-15 12:49:57 +0800

    Add first API call

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-import axios from "axios"
-import express from "express"
+import axios from "axios";
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
 
+dotenv.config();
 const app = express();
-const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", { 
+        content: "Waiting for data...",
+        initial: true, 
+    });
 });
 
-app.listen(port, () => {
-    console.log(`App running in port ${port}`);
+app.post("/fetchdata", async (req, res) => {
+    const query = req.body.city;
+    try {
+        // console.log(process.env.BASE_URL + "/current.json" + "?key=" + process.env.API_KEY + "&q=" + query);
+        const result = await axios.get(process.env.BASE_URL + "/current.json" + "?key=" + process.env.API_KEY + "&q=" + query);
+        res.render("index.ejs", { 
+            content: result.data.current.temp_c,
+            city: query,
+            initial: false,
+        });
+    } catch (error) {
+        res.status(404);
+        console.log("Error: ", error.message);
+    }
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`App running in port ${process.env.PORT}`);
 });
